d3952ae92eeec0a43c7fe432275bd892496bf744
AxelElRojo <axelzkai.123@gmail.com>
2021-02-17 14:36:00 -0600

    Added installer

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-#cp monitor.py crm
-#sudo chmod +x crm
-#cp crm ~/.local/bin/
-#cp config.py ~/.local/bin/
-#sudo ln -s ~/.local/bin/crm /bin/
+cp monitor.py crm
+sudo chmod +x crm
+cp crm ~/.local/bin/
+cp config.py ~/.local/bin/
+sudo ln -s ~/.local/bin/crm /bin/
 echo "All done!"
 read -p "Do you want to delete the cloned folder (Y/n): " ans
 if [ "$ans" == 'y' -o "$ans" == '' -o "$and" == 'Y' ]; then
